306494f659d8614f1002a9a79e0dd3df96c8cff1
Eduardo <EduardoAC@users.noreply.github.com>
2024-04-28 08:11:44 +0100

    Add rating component

---
+++ src/contentScript/components/Rating/Rating.tsx
@@ -0,0 +1,41 @@
+import "./Rating.css"
+import { useState } from "react"
+
+export function Rating() {
+    const [rating, setRating] = useState(null)
+    const [hover, setHover] = useState(null)
+    const [totalStars, setTotalStars] = useState(5)
+
+    return (
+        <div className="rating">
+            {[...Array(totalStars)].map((star, index) => {
+                const currentRating = index + 1
+
+                return (
+                    <label key={index}>
+                        <input
+                            key={star}
+                            type="radio"
+                            name="rating"
+                            value={currentRating}
+                            onChange={() => setRating(currentRating)}
+                        />
+                        <span
+                            className="star"
+                            style={{
+                                color:
+                                    currentRating <= (hover || rating)
+                                        ? "#ffc107"
+                                        : "#e4e5e9",
+                            }}
+                            onMouseEnter={() => setHover(currentRating)}
+                            onMouseLeave={() => setHover(null)}
+                        >
+                            &#9733;
+                        </span>
+                    </label>
+                )
+            })}
+        </div>
+    )
+}
