9cd3f0675822e5587b6d0efd1508be39acb8422e
Eduardo <EduardoAC@users.noreply.github.com>
2024-04-28 20:28:35 +0100

    Test utils

---
+++ src/contentScript/utils/shadow.test.tsx
@@ -0,0 +1,21 @@
+// createShadowRoot.test.ts
+import { render } from "@testing-library/react"
+import { createShadowRoot } from "./shadow"
+
+const cssURL = "https://example.com/styles.css"
+describe("createShadowRoot", () => {
+    it("should create a shadow root and append the React app root node", () => {
+        const { container } = render(<div id="app-root" />)
+        const root = container.querySelector("#app-root") as HTMLElement
+
+        const shadowEl = createShadowRoot(root, cssURL)
+
+        expect(shadowEl.shadowRoot).not.toBeNull()
+        const appRootInShadow = shadowEl.shadowRoot?.querySelector("#app-root")
+        expect(appRootInShadow).not.toBeNull()
+        const styleSheet = shadowEl.shadowRoot?.querySelector(
+            'link[rel="stylesheet"][href="https://example.com/styles.css"]',
+        )
+        expect(styleSheet).not.toBeNull()
+    })
+})
---
+++ src/contentScript/utils/url.test.ts
@@ -0,0 +1,27 @@
+import { clearUrlParams } from "./url"
+
+describe("clearUrlParams", () => {
+    it("should remove query parameters from the URL", () => {
+        const urlWithParams =
+            "http://example.com/page?param1=value1&param2=value2"
+        const expectedUrlWithoutParams = "http://example.com/page"
+        expect(clearUrlParams(urlWithParams)).toBe(expectedUrlWithoutParams)
+    })
+
+    it("should return the original URL if it doesn't contain query parameters", () => {
+        const urlWithoutParams = "http://example.com/page"
+        expect(clearUrlParams(urlWithoutParams)).toBe(urlWithoutParams)
+    })
+
+    it("should return the original URL if the input is not a string", () => {
+        const invalidUrl: any = null
+        expect(clearUrlParams(invalidUrl)).toBe(invalidUrl)
+    })
+
+    it("should handle URLs with hash fragments", () => {
+        const urlWithHash = "http://example.com/page#section"
+        expect(clearUrlParams(urlWithHash)).toBe(urlWithHash)
+    })
+
+    // Add more test cases to cover additional scenarios if needed
+})
